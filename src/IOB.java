2cf48d14a11f409a57a3ef7063fbfab5731c94e5
Bala Kumar <balaashok2004@gmail.com>
2025-05-07 10:59:15 +0530

    withdraw , deposit , Change bank name methods has been added

--- src/IOB.java
+++ src/IOB.java
@@ -2,7 +2,8 @@ public class IOB {
     static String bankName ="IndianOverseasBank";
     String name;
     double balance;
-    double amount;
+    static double interestRate =5.5;
+
 
     IOB(String name , double balance){
         this.name=name;
@@ -12,13 +13,30 @@ public class IOB {
     public void showInformation(){
         System.out.println("Customer name : " + name);
         System.out.println("The available balance : " + balance);
+        System.out.println(bankName);
     }
 
     public void deposit(double amount){
-        this.amount=amount;
         balance += amount;
         System.out.println("Deposited amount : " + amount);
         System.out.println("The available balance after deposit : " + balance);
+    }
+
+    public void withdraw(double amount){
+        if (amount<=balance){
+            balance = amount-balance;
+        }else {
+            System.out.println("insufficent balance");
+        }
+    }
+
+    public void printAnnualIntrest(){
+        double interest = (balance * interestRate) / 100;
+        System.out.println("Annual interest: " + interest);
+    }
+
+    public static void changeBankName(String newName) {
+        bankName = newName;
     }
 
 
@@ -26,5 +44,6 @@ public class IOB {
     public static void main(String[] args) {
         IOB bala = new IOB("Bala Kumar A " , 1000.00);
         bala.showInformation();
+        changeBankName("Mahindra bank");
     }
 }
